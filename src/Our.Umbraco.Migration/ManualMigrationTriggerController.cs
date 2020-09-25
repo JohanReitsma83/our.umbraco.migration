88b42b035b5dfefabfc8459d42d7537c4ccdc9a2
Benjamin Carleski <benjamin@proworks.com>
2020-09-24 17:38:53 -0700

    Add manual migration trigger

---
+++ src/Our.Umbraco.Migration/ManualMigrationTriggerController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Configuration;
+using Umbraco.Core.Logging;
+using Umbraco.Web.Editors;
+
+namespace Our.Umbraco.Migration
+{
+    public class ManualMigrationTriggerController : UmbracoAuthorizedJsonController
+    {
+        private const string TriggerKeyKey = "Our.Umbraco.Migration:ManualTriggerKey";
+        private static readonly string TriggerKey = ConfigurationManager.AppSettings[TriggerKeyKey];
+
+        public bool TriggerMigrations(string triggerKey)
+        {
+            if (TriggerKey == null)
+            {
+                Logger.Warn<ManualMigrationTriggerController>($"You must first set the {TriggerKeyKey} application setting");
+                return false;
+            }
+
+            if (triggerKey != TriggerKey)
+            {
+                Logger.Warn<ManualMigrationTriggerController>($"An incorrect triggerKey value was passed: {triggerKey}");
+                return false;
+            }
+
+            try
+            {
+                new MigrationApplier(ApplicationContext).ApplyNeededMigrations();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Logger.Error<ManualMigrationTriggerController>("Could not apply migrations", ex);
+                return false;
+            }
+        }
+    }
+}
